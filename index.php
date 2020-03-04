e3b80b2025b90e706c8b05076149e8c4e0c05cb4
helipatel19 <55427636+helipatel19@users.noreply.github.com>
2020-03-04 16:52:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <?php
-
+// create Product Interface and function to calculate product price
 interface Product
 {
     public function calculateProductPrice(int $productPrice, int $quanity);
 }
 
+// customer class is implementing Product Interface and calculateProductPrice method 
+// calculateProductPrice ommited the type int.
 class Customer implements Product
 {
 	// type omitted for $price and $quantity		
-    public function calculateProductPrice($price,$quanity){ 
-    	$total = $price * $quanity ;
-    	return $total;
+    	public function calculateProductPrice($price,$quanity){ 
+		$total = $price * $quanity ;
+		return $total;
 	}
 } 
 
 $customer = new Customer();
 
+// here we have passed float value and will not throw fatal error
 echo "total price is: ". $customer->calculateProductPrice(100.5,5);
 
 
